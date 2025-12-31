ad8286dbedde68c2ed0dae9f53c90655fceb47c1
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-31 13:40:55 +0700

    Update deployJava.groovy

--- vars/deployJava.groovy
+++ vars/deployJava.groovy
@@ -1,33 +1,34 @@
-def call () {
-     
-}
 def call() {
   stage('Deploy artifact on VM'){
-    if(env.BRANCH_NAME == 'releases'){
+     if(env.BRANCH_NAME == 'releases'){
       retry(2){
         timeout(time: 3, unit: 'MINUTES'){
-          withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]){
+          withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_ACC', passwordVariable: 'NEXUS_PASS')]){
             sshagent(["${PRIVKEY_SERV_VM}"]){
-              sh "ssh -o StrictHostKeyChecking=no root@${URL_SERV_VM} cp /opt/Board-game.jar /opt/Board-game2.jar"
-              sh "ssh root@${URL_SERV_VM} curl -v -u ${NEXUS_ACC} -o /opt/Board-game.jar http://${NEXUS_URL}/repository/${NEXUS_RELEASES_REPO}/${NEXUS_GROUP}/${NEXUS_ARTIFACT_ID}/${VERSION}/${NEXUS_ARTIFACT_ID}-${VERSION}-${env.BRANCH_NAME}"
+              sh "ssh -o StrictHostKeyChecking=no root@${URL_SERV_VM} cp /opt/${NEXUS_ARTIFACT_ID}.jar /opt/${NEXUS_ARTIFACT_ID}2.jar"
+              sh "ssh root@${URL_SERV_VM} curl -v -u ${NEXUS_ACC} -o /opt/${NEXUS_ARTIFACT_ID}.jar http://${NEXUS_URL}/repository/${NEXUS_RELEASES_REPO}/${NEXUS_GROUP}/${NEXUS_ARTIFACT_ID}/${VERSION}/${NEXUS_ARTIFACT_ID}-${VERSION}-${env.BRANCH_NAME}.jar"
             }
           }
         }
       }
-    }
-    
-   if(env.BRANCH_NAME.startsWith('uat')){
-     def GIT_HASH = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+     }
+       
+     if(env.BRANCH_NAME.startsWith('uat')){
+       def GIT_HASH = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
        retry(2){
          timeout(time: 3, unit: 'MINUTES'){
-           withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]){
+           withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_ACC', passwordVariable: 'NEXUS_PASS')]){
              sshagent(["${PRIVKEY_SERV_VM}"]){
-               sh "ssh -o s"
+               sh "ssh -o StrictHostKeyChecking=no root@${URL_SERV_VM} cp /opt/${NEXUS_ARTIFACT_ID}.jar /opt/${NEXUS_ARTIFACT_ID}2.jar"
+               sh "ssh root@${URL_SERV_VM} curl -v -u ${NEXUS_ACC} -o /opt/${NEXUS_ARTIFACT_ID}.jar http://${NEXUS_URL}/repository/${NEXUS_RELEASES_REPO}/${NEXUS_GROUP}/${NEXUS_ARTIFACT_ID}/${VERSION}/${NEXUS_ARTIFACT_ID}-${VERSION}-${env.BRANCH_NAME}-${GIT_HASH}.jar "
              }
            }
          }
        }
-   }
-    
+     }
+       
+     sshagent(["${PRIVKEY_SERV_VM}"]) {
+       sh "ssh root@${URL_SERV_VM} systemctl restart ${NEXUS_ARTIFACT_ID}"  
+    }
   }
 }
