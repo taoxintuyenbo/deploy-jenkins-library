29786092b911d87e2db7610fab33d6d09014cf70
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-31 14:22:54 +0700

    Add existence check before copying JAR file
    Updated the deployment script to check for the existence of the JAR file before copying it.

--- vars/deployJava.groovy
+++ vars/deployJava.groovy
@@ -5,21 +5,20 @@ def call() {
         timeout(time: 3, unit: 'MINUTES'){
           withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_ACC', passwordVariable: 'NEXUS_PASS')]){
             sshagent(["${PRIVKEY_SERV_VM}"]){
-              sh "ssh -o StrictHostKeyChecking=no root@${URL_SERV_VM} cp /opt/${NEXUS_ARTIFACT_ID}.jar /opt/${NEXUS_ARTIFACT_ID}2.jar"
+              sh "ssh -o StrictHostKeyChecking=no root@${URL_SERV_VM} 'test -f /opt/${NEXUS_ARTIFACT_ID}.jar && cp /opt/${NEXUS_ARTIFACT_ID}.jar /opt/${NEXUS_ARTIFACT_ID}2.jar'"
               sh "ssh root@${URL_SERV_VM} curl -v -u ${NEXUS_ACC} -o /opt/${NEXUS_ARTIFACT_ID}.jar http://${NEXUS_URL}/repository/${NEXUS_RELEASES_REPO}/${NEXUS_GROUP}/${NEXUS_ARTIFACT_ID}/${VERSION}/${NEXUS_ARTIFACT_ID}-${VERSION}-${env.BRANCH_NAME}.jar"
             }
           }
         }
       }
      }
-       
      if(env.BRANCH_NAME.startsWith('uat')){
        def GIT_HASH = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
        retry(2){
          timeout(time: 3, unit: 'MINUTES'){
            withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_ACC', passwordVariable: 'NEXUS_PASS')]){
              sshagent(["${PRIVKEY_SERV_VM}"]){
-               sh "ssh -o StrictHostKeyChecking=no root@${URL_SERV_VM} cp /opt/${NEXUS_ARTIFACT_ID}.jar /opt/${NEXUS_ARTIFACT_ID}2.jar"
+               sh "ssh -o StrictHostKeyChecking=no root@${URL_SERV_VM} 'test -f /opt/${NEXUS_ARTIFACT_ID}.jar && cp /opt/${NEXUS_ARTIFACT_ID}.jar /opt/${NEXUS_ARTIFACT_ID}2.jar'"
                sh "ssh root@${URL_SERV_VM} curl -v -u ${NEXUS_ACC} -o /opt/${NEXUS_ARTIFACT_ID}.jar http://${NEXUS_URL}/repository/${NEXUS_RELEASES_REPO}/${NEXUS_GROUP}/${NEXUS_ARTIFACT_ID}/${VERSION}/${NEXUS_ARTIFACT_ID}-${VERSION}-${env.BRANCH_NAME}-${GIT_HASH}.jar "
              }
            }
