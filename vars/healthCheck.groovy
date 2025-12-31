f26821d74feb9aba58044a0ba1c34f1b26eff052
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-31 15:26:02 +0700

    Update healthCheck.groovy

--- vars/healthCheck.groovy
+++ vars/healthCheck.groovy
@@ -4,7 +4,7 @@ def call() {
       sleep 30
       retry(3) {
         def r = httpRequest(
-          url: "http://${URL_SERV_VM}",
+          url: "http://${URL_SERV_VM}:8080",
           validResponseCodes: '200:399',
           timeout: 10
         )
