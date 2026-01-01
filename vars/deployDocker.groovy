daf6d48ff564026b2c1cff89f02b01e59833762d
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2026-01-01 16:09:00 +0700

    Create deployDocker.groovy

---
+++ vars/deployDocker.groovy
@@ -0,0 +1,27 @@
+def call(){
+  stage('Deploy Docker'){
+    if(env.BRANCH_NAME == 'release'){
+      withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_ACC', passwordVariable: 'NEXUS_PASS')]){
+        sshagent(["${PRIVKEY_SERV_VM}"]){
+          sh "ssh -o StrictHostKeyChecking=no root@${URL_SERV_VM} docker image prune -a -f"
+          sh "ssh root@${URL_SERV_VM} 'docker commit ${NEXUS_ARTIFACT_ID} ${NEXUS_URL_DOCKER}/docker-releases:stable'"
+          sh "ssh root@${URL_SERV_VM} 'echo ${NEXUS_PASS} | docker login -u ${NEXUS_ACC} --password-stdin ${NEXUS_URL_DOCKER}'"
+          sh "scp ./docker-compose.yaml root@${URL_SERV_VM}:/root/docker-compose.yaml"
+          sh "ssh root@${URL_SERV_VM} 'VERSION=${NEXUS_URL_DOCKER}/docker-releases:${VERSION}-${env.BRANCH_NAME} docker compose up -d --force-recreate'"
+        }
+      }
+    }
+    if(env.BRANCH_NAME.startsWith('uat')){
+      def GIT_HASH = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+      withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_ACC', passwordVariable: 'NEXUS_PASS')]){
+        sshagent(["${PRIVKEY_SERV_VM}"]){
+          sh "ssh -o StrictHostKeyChecking=no root@${URL_SERV_VM} docker image prune -a -f"
+          sh "ssh root@${URL_SERV_VM} 'docker commit ${NEXUS_ARTIFACT_ID} ${NEXUS_URL_DOCKER}/docker-releases:stable'"
+          sh "ssh root@${URL_SERV_VM} 'echo ${NEXUS_PASS} | docker login -u ${NEXUS_ACC} --password-stdin ${NEXUS_URL_DOCKER}'"
+          sh "scp ./docker-compose.yaml root@${URL_SERV_VM}:/root/docker-compose.yaml"
+          sh "ssh root@${URL_SERV_VM} 'VERSION=${NEXUS_URL_DOCKER}/docker-releases:${VERSION}-${env.BRANCH_NAME}-${GIT_HASH} docker compose up -d --force-recreate'"
+        }
+      }
+    }
+  }
+}
